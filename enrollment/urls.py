497d15c891bad2ec0f45073b9fb359fecb858437
Harry Amante <zerogold10000@gmail.com>
2025-11-22 16:14:57 +0800

    Delete enrollment directory

--- enrollment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('enrol/', views.enroll_user, name='enroll_user'),
-    path("enroll/", views.EnrollmentWizard.as_view(), name="enroll"),
-    
-
-]
